6b2eda93242e6b35d98898de3adaf049e2c1aafe
dawoodcyber <ds.khi.pk@gmail.com>
2024-03-25 09:53:09 +0500

    loops and conditions added

--- index.ts
+++ index.ts
@@ -1,23 +1,36 @@
-#! /usr/bin/env node 
+#! /usr/bin/env node
 import inquirer from "inquirer";
 // 1. computer will generate a number;
 // 2. taking input from user; 
 // 3. checkin Conditionn ;
-const computer_generated_number:number = Math.floor(Math.random() * 6 +1 );
+const computer_generated_number: number = Math.floor(Math.random() * 6 + 1);
 
-const input = await inquirer.prompt([
-    {
-        name: "Guessed_Number",
-        type: "number",
-        message: "Guess a number between 1-6",
+
+console.log("Welcome to Number Guessing Game");
+let count = 4;
+for (let i = 0; i < 5; i++) {
+    const input = await inquirer.prompt([
+        {
+            name: "Guessed_Number",
+            type: "number",
+            message: "Guess a number between 1-6",
+        }
+
+    ])
+
+    if (input.Guessed_Number == computer_generated_number) {
+        console.log("kudos! you guessed the number correctly");
+        break;
+    }
+    else {
+        if (count != 0)
+            console.log("Sorry! try again, incorrect guessed.");
     }
 
-])
-
-if(input.Guessed_Number == computer_generated_number)
-{
-    console.log("kudos! you guessed the number correctly");
-}
-else{
-    console.log("Sorry! try again, incorrect guessed.");
+    if (count == 0) {
+        console.log("Sorry you lose the game!");
+        break;
+    }
+    console.log(`${count} turns are left`);
+    count--;
 }
